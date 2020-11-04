d3fb18f524be72c4bfa1d06d18bda99f69dec27d
yujias424 <yj.shi@outlook.com>
2020-11-04 20:49:25 +0800

    Bug corrected

--- R/fetch_tcga_data.r
+++ R/fetch_tcga_data.r
@@ -21,7 +21,8 @@ fetch_tcga_data <- function(tcga_type,
                             data.type = data_type,
                             workflow.type = workflow_type,
                             sample.type = sample_type)
-  TCGAbiolinks::GDCdownload(g_query_tumor, directory = paste0(download_location, "/", tcga_type, "_tumor"))
+  TCGAbiolinks::GDCdownload(g_query_tumor, 
+                            directory = paste0(download_location, "/", tcga_type, "_tumor"))
   tumor <- TCGAbiolinks::GDCprepare(g_query_tumor, save = FALSE, 
                       directory = paste0(download_location, "/", tcga_type, "_tumor"))
   return(tumor)
